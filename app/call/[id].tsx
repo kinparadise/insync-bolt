8f53e22988d1455625052fde637e266bcdac86df
Amofa Newton <164711242+kinparadise@users.noreply.github.com>
2025-07-03 11:54:46 +0000

    Updated [id].tsx

--- app/call/[id].tsx
+++ app/call/[id].tsx
@@ -1174,7 +1174,7 @@ const createStyles = (theme: any) => StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    gap: 24,
+    gap: 20,
     marginBottom: 20,
   },
   controlButton: {
